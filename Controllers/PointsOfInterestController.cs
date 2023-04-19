df865b1da2adca1d4cf79da32fdce05b19c46f1b
Sumit Ranjan <jcsumit917@gmail.com>
2023-04-19 18:23:31 +0530

    Post method added to create point of interest

--- Controllers/PointsOfInterestController.cs
+++ Controllers/PointsOfInterestController.cs
@@ -16,7 +16,7 @@ namespace CityInfo.API.Cotrollers
             }
             return Ok(city.PointsOfInterest);
         }
-        [HttpGet("{pointofinterestid}")]
+        [HttpGet("{pointofinterestid}", Name = "GetPointofinterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointofinterestid)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c=>c.Id == cityId);
@@ -32,5 +32,34 @@ namespace CityInfo.API.Cotrollers
             }
             return Ok(pointOfInterest);
         }
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointofInterest(
+            int cityId, PointOfInterestForCreationDto pointOfInterest)
+            {
+                var city = CitiesDataStore.Current.Cities
+                .FirstOrDefault(c => c.Id == cityId);
+                if(city == null)
+                {
+                    return NotFound();
+                }
+                //demo purpose - to be improved
+                var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                    c => c.PointsOfInterest).Max(p => p.Id);
+                var finalPointOfInterest = new PointOfInterestDto()
+                {
+                    Id = ++maxPointOfInterestId,
+                    Name = pointOfInterest.Name,
+                    Description = pointOfInterest.Description
+                };
+                city.PointsOfInterest.Add(finalPointOfInterest);
+                return CreatedAtRoute("GetPointOfInterest", 
+                new
+                {
+                    cityId = cityId,
+                    pointOfInterestId = finalPointOfInterest.Id
+                },
+                finalPointOfInterest
+                );
+            }
     }
 }
