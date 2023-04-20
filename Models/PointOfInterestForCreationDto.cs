671954b2ab588fd0930aae9020a0f356416c2876
Sumit Ranjan <jcsumit917@gmail.com>
2023-04-20 19:25:32 +0530

    Attributes added for basic validation

--- Models/PointOfInterestForCreationDto.cs
+++ Models/PointOfInterestForCreationDto.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Models
 {
     public class PointOfInterestForCreationDto
     {
+        [Required(ErrorMessage = "You should provide a name")]
+        [MaxLength(50)]
         public string Name { get; set;}
+        [MaxLength(200)]
         public string? Description { get; set;}
     }
 }
