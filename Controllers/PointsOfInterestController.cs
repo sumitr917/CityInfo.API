2e2b6b5a91dc23f7516f3bc9d2c2805a18eee289
Sumit Ranjan <jcsumit917@gmail.com>
2023-04-27 14:34:08 +0530

    implemented Dependecy Injection

--- Controllers/PointsOfInterestController.cs
+++ Controllers/PointsOfInterestController.cs
@@ -1,4 +1,6 @@
 using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Cotrollers
@@ -7,19 +9,40 @@ namespace CityInfo.API.Cotrollers
     [ApiController]
     public class PointsOfInterestController : ControllerBase
     {
+        private readonly ILogger<PointsOfInterestController> _logger;
+        private readonly IMailService _mailSerive;
+        private readonly CitiesDataStore _citiesDataStore;
+
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger,
+        IMailService mailService, CitiesDataStore citiesDataStore)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mailSerive = mailService ?? throw new ArgumentNullException(nameof(mailService));
+            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+        }
         [HttpGet]
         public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c=>c.Id==cityId);
-            if(city==null){
-                return NotFound();
+            try
+            {
+                var city = _citiesDataStore.Cities.FirstOrDefault(c=>c.Id==cityId);
+                if(city==null){
+                    _logger.LogInformation("City with city Id {0} was not found.",cityId);
+                    return NotFound();
+                }
+                return Ok(city.PointsOfInterest);
             }
-            return Ok(city.PointsOfInterest);
+            catch(Exception ex)
+            {
+                _logger.LogCritical($"Exception while getting points of interest for city with id {cityId}.", ex);
+                return StatusCode(500, "A problem while handling your request");
+            }
+            
         }
         [HttpGet("{pointofinterestid}", Name = "GetPointofinterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointofinterestid)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c=>c.Id == cityId);
+            var city = _citiesDataStore.Cities.FirstOrDefault(c=>c.Id == cityId);
             if(city==null)
             {
                 return NotFound();
@@ -37,14 +60,14 @@ namespace CityInfo.API.Cotrollers
             int cityId, PointOfInterestForCreationDto pointOfInterest)
 
             {
-                var city = CitiesDataStore.Current.Cities
+                var city = _citiesDataStore.Cities
                 .FirstOrDefault(c => c.Id == cityId);
                 if(city == null)
                 {
                     return NotFound();
                 }
                 //demo purpose - to be improved
-                var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                var maxPointOfInterestId = _citiesDataStore.Cities.SelectMany(
                     c => c.PointsOfInterest).Max(p => p.Id);
                 var finalPointOfInterest = new PointOfInterestDto()
                 {
@@ -66,7 +89,7 @@ namespace CityInfo.API.Cotrollers
         public ActionResult UpdatePointOfInterest(int cityId, int pointofinterestid, 
         PointOfInterestForUpdateDto pointOfInterest)
         {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            var city = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == cityId);
             if(city == null)
             {
                 return NotFound();
@@ -81,5 +104,61 @@ namespace CityInfo.API.Cotrollers
 
             return Ok();
         }
+        [HttpPatch("{pointofinterestid}")]
+        public ActionResult PartiallyUpdatePointOfInterest(int cityId, int pointofinterestid,
+        JsonPatchDocument<PointOfInterestForUpdateDto> patchDocument)
+        {
+            var city = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointofinterestid);
+            if(pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterestToPatch = new PointOfInterestForUpdateDto()
+            {
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+            patchDocument.ApplyTo(pointOfInterestToPatch, ModelState);
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if(!TryValidateModel(pointOfInterestToPatch))
+            {
+                return BadRequest(ModelState);
+            }
+
+            pointOfInterest.Name = pointOfInterestToPatch.Name;
+            pointOfInterest.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{pointofinterestid}")]
+        public ActionResult DeletePointOfInterest(int cityId, int pointofinterestid)
+        {
+            var city = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointofinterestid);
+            if(pointOfInterest == null)
+            {
+                return NotFound();
+            }
+            city.PointsOfInterest.Remove(pointOfInterest);
+            _mailSerive.Send(
+                "Point of interest deleted.",
+                $"Point of interest {pointOfInterest.Name} with id {pointOfInterest.Id} was deleted."
+            );
+            return NoContent();
+        }
+
     }
 }
